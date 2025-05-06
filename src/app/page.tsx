99e00378a02dcdbd81217de9fcfd984654924085
FazaleAli <fazaleali56@gmail.com>
2025-05-07 00:31:31 +0500

    done with the some customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
  return(
-  <Button variant={"destructive"}>
-    Click me
-  </Button>
+  <div className="p-5">
+  <div className="flex flex-col gap-y-4">
+    <div className="w-[50%] gap-y-6 flex flex-col">
+      <Input placeholder="hello" ></Input>
+      <Textarea placeholder="hello" ></Textarea>
+      <Checkbox/>
+      <Progress value={50} />
+      <Button variant={"elevated"}>Hello</Button>
+      </div>
+  </div>
+  </div>
  )
 }
